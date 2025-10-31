0bd3c889f273dbb49dfc14c8fe8eb418a49a1c6c
mojjammil <mojjammil@gmail.com>
2025-10-31 16:44:59 +0200

    updated the external id

--- src/mapping/external-ids.ts
+++ src/mapping/external-ids.ts
@@ -2,11 +2,10 @@ export type City = 'milano' | 'roma' | 'torino';
 
 /**
  * External ID mapping by city
- * These are hardcoded for now
  */
 export const EXTERNAL_IDS: Record<City, string> = {
   milano: '32727_4',
-  roma: '32727_4',
-  torino: '32727_4',
+  roma: '20944_0',
+  torino: '20954_0',
 };
 
